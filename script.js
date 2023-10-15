28e9829a0e509409feb97a427a6a10d830e49474
adiamondplumdot <ariel.diamond@gmail.com>
2023-10-15 10:18:04 -0400

    connect nudges

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ function getRandomInt(max) {
 }
 
 async function fetchData() {
-    fetch('./nudges.json')
-        .then((response) => response.json())
-        .then((json) => console.log(json));
+    const num = getRandomInt(36)
+    const response = await fetch('https://raw.githubusercontent.com/arieldiamond/plumdot-nudge/main/nudges.json', {method: 'GET', 'Access-Control-Allow-Origin': '*' })
+    const result = await response.json()
+    document.getElementById('reminder').innerHTML = result[num].Prompt
 }
 
 fetchData();
